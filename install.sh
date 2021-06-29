3233703028311dfe21665e44b207cdcf596c32da
x62\x30\x74\x68 <43070774+b0th@users.noreply.github.com>
2021-06-29 12:44:22 +0200

    Better install script

--- install.sh
+++ install.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
-   
-pkg=(pacman apt dnf yum zipper)
-c=False
-for x in ${pkg[*]}; do
-    if [ $(which $x) ];then
-    sudo $x install python3 python3-pip
-    c=True
+
+# Check this packages
+pkg=(python3)
+pkg_manager=(apt brew)
+
+# Check your default package manager and install packages
+install_pkg () {
+    if [[ $(which $1) && ! "not found" =~ $(which $1) ]]; then
+        :
+    else
+        for manager in ${pkg_manager[*]}; do
+            [[ $(which $manager) ]] && sudo $manager install $1 && return
+        done
     fi
-done
+}
 
-if [ $c != True ];then exit;fi
+# Installing all packages that you dont have
+install_all_pkg () {
+    for x in ${pkg[*]}; do 
+        install_pkg $x
+    done
+}
 
-echo -e "\n\e[32mInstall requirements ?\e[0m [Y/n]"
-read ask
-
-if [ $ask == "Y" ] || [ $ask == "y" ];then
-pip3 install -r requirements.txt
-fi
+# Main function
+main () {
+    install_all_pkg
+}
